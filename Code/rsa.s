7279e9e37fc9703c37d87a939ceb5ecd7ebddd62
Mark Jia <mjia@ualberta.ca>
2023-01-23 15:47:33 -0600

    init

---
+++ Code/rsa.s
@@ -0,0 +1,43 @@
+#
+# CMPUT 229 Student Submission License
+# Version 1.0
+#
+# Copyright 2019 <student name>
+#
+# Redistribution is forbidden in all circumstances. Use of this
+# software without explicit authorization from the author or CMPUT 229
+# Teaching Staff is prohibited.
+#
+# This software was produced as a solution for an assignment in the course
+# CMPUT 229 - Computer Organization and Architecture I at the University of
+# Alberta, Canada. This solution is confidential and remains confidential 
+# after it is submitted for grading.
+#
+# Copying any part of this solution without including this copyright notice
+# is illegal.
+#
+# If any portion of this software is included in a solution submitted for
+# grading at an educational institution, the submitter will be subject to
+# the sanctions for plagiarism at that institution.
+#
+# If this software is found in any public website or public repository, the
+# person finding it is kindly requested to immediately report, including 
+# the URL or other repository locating information, to the following email
+# address:
+#
+#          cmput229@ualberta.ca
+#
+#---------------------------------------------------------------
+# CCID:                 
+# Lecture Section:      
+# Instructor:           J. Nelson Amaral
+# Lab Section:          
+# Teaching Assistant:   
+#---------------------------------------------------------------
+# 
+
+.include "common.s"
+
+#----------------------------------
+#        STUDENT SOLUTION
+#----------------------------------
